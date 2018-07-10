02d94934dd45d1f56d3a345043f10fccbaeed911
Satish Venkatakrishna <satish860@gmail.com>
2018-07-10 14:05:34 +0530

    Add project files.

---
+++ KaggleTitanic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KaggleTitanic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
